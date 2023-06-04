e2c447d8b4f9663f4650b00662c99fc621dbb442
Hazey8709 <jfortin802@gmail.com>
2023-06-03 23:37:49 -0400

    first

---
+++ css-styles-1/notes.js
@@ -0,0 +1,11 @@
+//!           NOTES
+
+
+//* Covers CSS setup and shows different ways to use CSS in html.
+
+//* We go over inline, internal & external CSS. We will go over a little bit of color by using Class & Id.
+
+//* Will go over Class and ID to target html elements.
+
+//* Use One CSS file for 2 html files.
+
---
+++ styles-2/notes.js
@@ -0,0 +1,12 @@
+//!        NOTES
+
+//* Color Hex #0005, RGB(222, 32, 67), hsl(122,80%, 45%)
+
+//* Font family
+//* font size
+//* font weight
+//* font style
+
+//* Border styles
+//* Border radius
+//* Left, right, top, bottom borders
