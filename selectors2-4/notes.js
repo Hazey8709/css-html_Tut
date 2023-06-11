f37f623d11f0ba87da5f93b89310b49429649d8e
Hazey8709 <jfortin802@gmail.com>
2023-06-10 22:42:00 -0400

    update

---
+++ selectors2-4/notes.js
@@ -0,0 +1,11 @@
+//!        JS   File
+
+//!         NOTES
+
+//* :not Selector.
+//* :nth-child(n).
+//* :nth-last-child(n).
+//* :nth-last-of-type(n).
+//* *  - Universal Operator.
+//* Descendant Combinator (.box p{}).
+//* Example of Parent, Child, Grand Child, relations.
