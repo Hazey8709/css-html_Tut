b78da7631f918179ea2528f186e7ee8f91951d01
Hazey8709 <jfortin802@gmail.com>
2023-06-25 22:00:16 -0400

    updated files

---
+++ quickKeys-5/notes.js
@@ -0,0 +1,12 @@
+//!    Notes
+
+//* HTML & CSS quick keys are provided in cheat sheet.
+
+//* Covered in lesson:---------------------------
+//* #header, .title, p.class1.class2.class3,
+//* ul>li*5, ul>li{listItem}*5, ul>li.sample$*5, h$[title=topic$]{Headline $}*3,
+//* ul>li.item$$$*5, ul>li.item$@-*5, ul>li.item$@3*5, p[title="Hello World"],
+//* a{Click Me}, p>{Click }+a{here}+{ to continue}, a, a:link, a:mail, link,
+//* link:css,  meta:utf (Meta-UTF Code), meta:vp (Meta-Viewport), meta:compat
+
+//* rest is in File on Github.
