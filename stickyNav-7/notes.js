fb6d0ccf7ec47f202cde9a917f3f775d93c53a94
Hazey8709 <jfortin802@gmail.com>
2023-06-29 19:19:24 -0400

    ud

---
+++ stickyNav-7/notes.js
@@ -0,0 +1,19 @@
+//!                     Notes /  JS
+
+
+//* Sticky Nav using HTML & CSS.
+//* Nav will transform on scroll.
+//* Responsive 450-500px - 1750px
+//* ( Large cell Phone /Small tablet up to laptop)
+
+
+var navbar = document.getElementById("navbar");
+var menu = document.getElementById("menu");
+
+window.onscroll = function () {
+    if (window.pageYOffset >= menu.offsetTop) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+    }
+};
