afab315fe79ad0f0b47d28c73fc83c6b25636bc4
Hazey8709 <jfortin802@gmail.com>
2023-07-06 18:54:52 -0400

    added 4 projects

---
+++ pagination-8/notes.js
@@ -0,0 +1,6 @@
+//!         Notes
+
+//* We use HTML & CSS to navigate pages in the form of Pagination.
+//* No JavaScript needed Only HTML & CSS To switch pages by clicking page number or the arrows to go forward or backwards through the pages.
+
+//* Different method of navigation for HTML.
---
+++ semantics-10/notes.js
@@ -0,0 +1,11 @@
+//!            Notes:
+
+//* We build a Web page layout template using semantics.
+//* HTML Semantic Elements Naming Convention:
+// ( HTML semantics are the use of HTML elements to convey the meaning and
+//structure of the content, rather than just the presentation.Semantic HTML
+//helps browsers, search engines, screen readers, and other web services
+//understand the content better.It also makes the code more readable and
+//maintainable for developers.)
+
+//* Header, Nav, Main(aside, section, article), Footer.
