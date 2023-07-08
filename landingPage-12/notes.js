47ecacaed9cc80875b6f8d7ece2110eb836fc0fa
Hazey8709 <jfortin802@gmail.com>
2023-07-07 23:16:07 -0400

    ud

--- landingPage-12/notes.js
+++ landingPage-12/notes.js
@@ -3,6 +3,8 @@
 //* We build a simple website entry page using only html and css. Could also
 //* be a landing page or modified for something similar.
 
-//*
+//* Using divs I make a design for the background then put an overlay
+//* blackout with title name and enter button. Simple yet works perfectly and
+//* serves it's purpose.
 
-//* Code: 
+//* Code: https://github.com/Hazey8709/css-html_Tut/tree/main/landingPage-12
