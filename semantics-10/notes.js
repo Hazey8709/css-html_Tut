09d28cb4022784beac852c9899793cd33cb6a76d
Hazey8709 <jfortin802@gmail.com>
2023-07-09 19:41:53 -0400

    ud

--- semantics-10/notes.js
+++ semantics-10/notes.js
@@ -2,10 +2,15 @@
 
 //* We build a Web page layout template using semantics.
 //* HTML Semantic Elements Naming Convention:
-// ( HTML semantics are the use of HTML elements to convey the meaning and
-//structure of the content, rather than just the presentation.Semantic HTML
-//helps browsers, search engines, screen readers, and other web services
-//understand the content better.It also makes the code more readable and
-//maintainable for developers.)
+// (
+//   HTML semantics are the use of HTML elements to convey the meaning and
+// structure of the content, rather than just the presentation.Semantic HTML
+// helps browsers, search engines, screen readers, and other web services
+// understand the content better.It also makes the code more readable and
+// maintainable for developers.
+// )
 
 //* Header, Nav, Main(aside, section, article), Footer.
+//* Responsive down to Width: 375px
+
+//* Code: https://github.com/Hazey8709/css-html_Tut/tree/main/semantics-10
