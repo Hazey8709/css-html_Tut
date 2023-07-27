77ca8b43eaf23e169ed8b6e5e01775b7b4529ffc
Hazey8709 <jfortin802@gmail.com>
2023-07-26 22:50:49 -0400

    ud

---
+++ backgrounds-13/notes.js
@@ -0,0 +1,17 @@
+//t           Notes
+
+// background-image: url("#");
+// background-repeat: no-repeat;
+//  background-attachment: fixed;
+//  background-position: center;
+//  background-size: cover;
+
+// background: linear-gradient(to right,skyblue, white);
+//  background-repeat: no-repeat;
+// background - attachment: fixed;
+
+// background: linear-gradient(skyblue, white);
+//  background-repeat: no-repeat;
+//  background-attachment: fixed;
+
+//t          Js
